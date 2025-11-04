618f0ef67b36dab9ce4cf90dfc2a4e11011518aa
javirg_22 <javiretuerta2004@gmail.com>
2025-11-04 13:15:46 +0100

    actualicacion1

--- app/src/main/java/com/example/nicestart/login.java
+++ app/src/main/java/com/example/nicestart/login.java
@@ -1,6 +1,8 @@
 package com.example.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void openMain(View v){
+        Intent intent = new Intent(login.this,MainActivity.class) ;
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
