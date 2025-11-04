bf0079db7239fd3b6ea9776e31e4f19927c475e7
javirg_22 <javiretuerta2004@gmail.com>
2025-11-04 13:30:52 +0100

    creacion signup

--- app/src/main/java/com/example/nicestart/signup.java
+++ app/src/main/java/com/example/nicestart/signup.java
@@ -10,27 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void openMain(View v){
-        Intent intent = new Intent(login.this,MainActivity.class) ;
+    public void openMain(View v) {
+        Intent intent = new Intent(signup.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
-    public void openSignup(View v){
-        Intent intent = new Intent(login.this,signup.class) ;
+    public void openLogin(View v) {
+        Intent intent = new Intent(signup.this, login.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
