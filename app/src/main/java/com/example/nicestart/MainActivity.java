ac25811b64b1da38500d400a811e16c61d3616e4
javirg_22 <javiretuerta2004@gmail.com>
2025-11-20 13:33:32 +0100

    menus y actualizacion del readme

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.nicestart;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView mycontext =findViewById(R.id.main2);
+        registerForContextMenu(mycontext);
+        swipeLayout= findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(monRefreshListener);
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if (item.getItemId()==R.id.item1){
+            Toast toast = Toast.makeText(this,"item copied",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        }else if (item.getItemId()==R.id.item2){
+            Toast toast2 = Toast.makeText(this,"Downloading item ...",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false ;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_appbar,menu);
+        return true;
+    }
+   // @Override
+   // public boolean onOptionsItemSelected(MenuItem menu){
+       // int id = item.getItemId();
+   // }
+    protected SwipeRefreshLayout.OnRefreshListener
+            monRefreshListener= new SwipeRefreshLayout.OnRefreshListener() {
+       @Override
+       public void onRefresh() {
+           Toast toast0= Toast.makeText(MainActivity.this,"Hi there!",Toast.LENGTH_LONG);
+           toast0.show();
+           swipeLayout.setRefreshing(false);
+       }
+   };
 }
